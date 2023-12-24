b6227760fd5376e5eec44f642e343492e122d360
Vineet-0 <vineet351016@gmail.com>
2023-12-24 17:57:32 +0530

    Basic styling and logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState, useEffect } from "react";
 
 import { MdRestaurantMenu } from "react-icons/md";
-import { FaUserCircle } from "react-icons/fa";
+// import { FaUserCircle } from "react-icons/fa";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 import { FaChevronDown } from "react-icons/fa";
+import { MdOutlineMenu } from "react-icons/md";
+
 
 import data from "./components/data.jsx";
 import ListCard from "./components/ListCard.jsx";
+import HeavenMealLogo from "./assets/HeavenMealLogo.png"
 
 function App() {
     const categoryNames = [
@@ -14,61 +18,67 @@ function App() {
         "Biryani", "Momos", "Combos", "Rice", "Breads", "Desserts",
     ];
       const [visibleCategory, setVisibleCategory] = useState(null);
+      const [searchBar, setSearchBar] = useState('false');
 
       const toggleVisibility = (category) => {
         setVisibleCategory((prevCategory) => (prevCategory === category ? null : category));
       };
 
     return (
-        <div className="w-full h-auto  no-scrollbar">
+        <div className="w-full h-auto  no-scrollbar bg-hero backdrop-blur">
             {/* Navbar */}
-            <div className="flex flex-col w-full">
+            <div className="flex flex-col w-full ">
             {/* <div className="z-1 fixed flex flex-col w-full"> */}
-                <div className="bg-red-400 w-full px-4 sm:px-16 py-6 flex flex-row items-center justify-between">
-                <div className="text-2xl font-bold">
-                    <MdRestaurantMenu />
+                <div className="w-full px-8 sm:px-16 py-2 flex flex-row items-center justify-between bg-[#f1f1f1] shadow">
+                    <div className="text-3xl font-bold">
+                        <MdOutlineMenu />
+                    </div>
+                    <div>
+                        <img className="w-[190px]" src={HeavenMealLogo} alt="Heaven Meal Logo" />
+                    </div>
+                    <div className="text-2xl font-bold"
+                        onClick = {() => setSearchBar(!searchBar)}>
+                        <FaMagnifyingGlass />
+                    </div>
                 </div>
-                <div className="text-2xl font-bold">Heaven Meal</div>
-                <div className="text-2xl font-bold">
-                    <FaUserCircle />
-                </div>
-                </div>
-                {/* <div className="bg-yellow-300 flex flex-row items-center justify-center w-full py-2">
-                <div className="bg-white flex flex-row items-center justify-center gap-2 w-full sm:w-3/4 lg:w-1/2 mx-4 lg:mx-0 px-4 py-2 rounded-md">
-                    <input type="text" className="w-full" />
-                    <button className="text-black">
-                    <FaMagnifyingGlass />
-                    </button>
-                </div>
-                </div> */}
+                {searchBar && 
+                    <div className="flex flex-row items-center justify-center w-full pt-4">
+                        <div className="bg-[#f1f1f1] shadow flex flex-row items-center justify-center gap-2 w-full sm:w-3/4 lg:w-1/2 mx-4 lg:mx-0 px-4 py-2 rounded-md">
+                            <input type="text" placeholder="What would you like to eat today?" className="w-full bg-[#f1f1f1] text-black" />
+                                <button className="text-black">
+                                <FaMagnifyingGlass />
+                            </button>
+                        </div>
+                    </div>
+                }
+                
             </div>
 
             {/* Main Menu */}
-            <div className="w-full px-4 sm:px-16 pt-4 pb-8">
-            {/* <div className="w-full px-4 sm:px-16 pt-36 pb-8"> */}
-                <div className="flex flex-col gap-4">
-                {categoryNames.map((name,index) => (
-                    <div key={index} className="w-full">
-                        <div onClick={() => toggleVisibility(name)} className="border rounded-md">
-                            <div className="flex flex-row items-center justify-between px-4 py-2">
-                                {name}
-                                <FaChevronDown />
+            <div className="w-full px-4 sm:px-16 pt-2 pb-8">
+                <div className="flex flex-col">
+                    {categoryNames.map((name,index) => (
+                        <div key={index} className="w-full">
+                            <div onClick={() => toggleVisibility(name)} className="bg-white border rounded-md shadow my-2">
+                                <div className="flex flex-row items-center justify-between px-5 py-2">
+                                    <div className="text-xl font-gagalin">{name}</div>
+                                    <FaChevronDown />
+                                </div>
+                            </div>
+                            
+                            <div className="px-2 h-fit">
+                                {visibleCategory === name && 
+                                    <div className="w-full flex flex-col gap-2">
+                                        {data.filter(item => item.category_name === name).map((item, index) => (
+                                        <div key={index} className="bg-white shadow">
+                                            <ListCard item={item} index={index} />
+                                        </div>
+                                        ))}
+                                    </div>
+                                }
                             </div>
                         </div>
-                        
-                        <div>
-                            {visibleCategory === name && 
-                                <div className="w-full flex flex-col gap-2 pt-2">
-                                    {data.filter(item => item.category_name === name).map((item, index) => (
-                                    <div key={index}>
-                                        <ListCard item={item} index={index} />
-                                    </div>
-                                    ))}
-                                </div>
-                            }
-                        </div>
-                    </div>
-                ))}
+                    ))}
                 </div>
             </div>
         </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend:{
+      backgroundImage: {
+        'hero': "url('./assets/backgroundImage.jpg')",
+      },
+      fontFamily: {
+        gagalin: ['Gagalin'],
+      },
+    },
   },
   plugins: [],
 }
